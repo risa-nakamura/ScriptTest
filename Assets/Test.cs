7a84880b6fc1e09d8daea8838c80019e88f23b64
risa-nakamura <negi39390822nasi@gmail.com>
2020-02-06 01:58:44 +0900

    スクリプトの基礎 配列

---
+++ Assets/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        int[] array = { 9, 7, 2, 5, 8 };
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (int i = array.Length-1; 0 <= i; i--)
+        {
+            Debug.Log(array [i]);
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Assets/Test2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int hp = 100;
+    private int power = 25;
+    private int mp = 53;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        this.hp -= damage;
+    }
+
+    public void Magic()
+    {
+        for (int mp = 0; mp < 5; mp-=5)
+        {
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+        }
+        if (mp <= 5)
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
+
+
+
+
+public class Test2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Boss lastboss = new Boss();
+
+        lastboss.Attack();
+
+        lastboss.Defence(3);
+
+        lastboss.Magic();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
